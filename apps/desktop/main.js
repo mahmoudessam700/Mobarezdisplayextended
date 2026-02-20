dedcef515973b05e4bc7359b62744da1c7c14483
mahmoudessam700 <mahmoudessam700@gmail.com>
2026-02-21 00:28:21 +0200

    feat: Migrate remote input simulation from robotjs to nut.js, updating input handling and dependencies accordingly.

--- apps/desktop/main.js
+++ apps/desktop/main.js
@@ -49,80 +49,86 @@ function createWindow() {
         mainWindow.webContents.openDevTools();
     }
 
-    // Remote Input Simulation using robotjs (cross-platform)
-    const robot = require('robotjs');
+    // Remote Input Simulation using nut.js (cross-platform, native pre-builds)
+    // const { mouse, keyboard, Key, Point, screen: nutScreen, Button } = require('@nut-tree-fork/nut-js');
+
+    // Remove delay for faster remote control
+    nutScreen.config.resourceDirectory = process.cwd();
+    mouse.config.mouseSpeed = 2000;
 
     // Track last known mouse position for click events
     let lastMouseX = 0;
     let lastMouseY = 0;
 
-    ipcMain.on('simulate-input', (event, data) => {
+    ipcMain.on('simulate-input', async (event, data) => {
         console.log('[ELECTRON-IPC] Received simulate-input:', data.type);
 
         try {
-            const screenSize = robot.getScreenSize();
-            const { width, height } = screenSize;
+            const width = await nutScreen.width();
+            const height = await nutScreen.height();
 
             if (data.type === 'mousemove') {
                 const absX = Math.round(data.x * width);
                 const absY = Math.round(data.y * height);
                 lastMouseX = absX;
                 lastMouseY = absY;
-                robot.moveMouse(absX, absY);
+                await mouse.setPosition(new Point(absX, absY));
             } else if (data.type === 'mousedown') {
                 const absX = data.x !== undefined ? Math.round(data.x * width) : lastMouseX;
                 const absY = data.y !== undefined ? Math.round(data.y * height) : lastMouseY;
-                console.log(`[ELECTRON-IPC] clicking at: ${absX}, ${absY}`);
-                robot.moveMouse(absX, absY);
-                robot.mouseToggle('down');
+                if (data.x !== undefined || data.y !== undefined) {
+                    await mouse.setPosition(new Point(absX, absY));
+                }
+                const btn = data.button === 2 ? Button.RIGHT : data.button === 1 ? Button.MIDDLE : Button.LEFT;
+                await mouse.pressButton(btn);
             } else if (data.type === 'mouseup') {
                 const absX = data.x !== undefined ? Math.round(data.x * width) : lastMouseX;
                 const absY = data.y !== undefined ? Math.round(data.y * height) : lastMouseY;
-                robot.moveMouse(absX, absY);
-                robot.mouseToggle('up');
+                if (data.x !== undefined || data.y !== undefined) {
+                    await mouse.setPosition(new Point(absX, absY));
+                }
+                const btn = data.button === 2 ? Button.RIGHT : data.button === 1 ? Button.MIDDLE : Button.LEFT;
+                await mouse.releaseButton(btn);
             } else if (data.type === 'keydown') {
                 console.log('[ELECTRON-IPC] keydown:', data.key);
                 const key = data.key;
 
-                // Build modifier array for robotjs
-                const modifiers = [];
-                if (data.modifiers?.shift) modifiers.push('shift');
-                if (data.modifiers?.ctrl || data.modifiers?.control) modifiers.push('control');
-                if (data.modifiers?.alt) modifiers.push('alt');
-                if (data.modifiers?.meta) modifiers.push('command');
-
-                // Map special keys to robotjs key names
+                // Map special keys to Nut.js Key enum
                 const keyMap = {
-                    'Enter': 'enter',
-                    'Return': 'enter',
-                    'Escape': 'escape',
-                    'Tab': 'tab',
-                    'Backspace': 'backspace',
-                    'Delete': 'delete',
-                    'ArrowUp': 'up',
-                    'ArrowDown': 'down',
-                    'ArrowLeft': 'left',
-                    'ArrowRight': 'right',
-                    'Space': 'space',
-                    ' ': 'space',
-                    'Home': 'home',
-                    'End': 'end',
-                    'PageUp': 'pageup',
-                    'PageDown': 'pagedown',
-                    'F1': 'f1', 'F2': 'f2', 'F3': 'f3', 'F4': 'f4',
-                    'F5': 'f5', 'F6': 'f6', 'F7': 'f7', 'F8': 'f8',
-                    'F9': 'f9', 'F10': 'f10', 'F11': 'f11', 'F12': 'f12',
+                    'Enter': Key.Enter,
+                    'Return': Key.Enter,
+                    'Escape': Key.Escape,
+                    'Tab': Key.Tab,
+                    'Backspace': Key.Backspace,
+                    'Delete': Key.Delete,
+                    'ArrowUp': Key.Up,
+                    'ArrowDown': Key.Down,
+                    'ArrowLeft': Key.Left,
+                    'ArrowRight': Key.Right,
+                    'Space': Key.Space,
+                    ' ': Key.Space,
+                    'Home': Key.Home,
+                    'End': Key.End,
+                    'PageUp': Key.PageUp,
+                    'PageDown': Key.PageDown,
+                    'Shift': Key.LeftShift,
+                    'Control': Key.LeftControl,
+                    'Alt': Key.LeftAlt,
+                    'Meta': Key.LeftSuper,
                 };
 
-                const robotKey = keyMap[key] || key.toLowerCase();
+                // Function keys
+                if (/^F[1-9]$|^F1[0-2]$/.test(key)) {
+                    keyMap[key] = Key[`F${key.substring(1)}`];
+                }
 
-                if (modifiers.length > 0) {
-                    robot.keyTap(robotKey, modifiers);
+                const nutKey = keyMap[key];
+
+                if (nutKey) {
+                    await keyboard.type(nutKey);
                 } else if (key.length === 1) {
-                    // For single characters, use typeString for better compatibility
-                    robot.typeString(key);
-                } else {
-                    robot.keyTap(robotKey);
+                    // Single printable character
+                    await keyboard.type(key);
                 }
             }
         } catch (err) {
